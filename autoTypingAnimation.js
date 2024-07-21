dcf9f8450e2575490e2e01eab7b5bbc28eb24266
Swagatam Adhikary <133666171+swagatamadhikary018@users.noreply.github.com>
2024-07-21 19:06:44 +0530

    Add files via upload

---
+++ autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span> 👨‍💻 BORN TO CODE FORCED TO WORK 👨‍💻!! Focused On Work. Hii Everyone, Myself Swagatam Adhikary. I am pursuing BCA(2021-2024) from Techno India (Hooghly Campus) which is affiliated by MAKAUT University. I'm absolutely grateful to introduce myself and share my thoughts here. I was born in Singur, Hooghly, West Bengal,India.I started my schooling from Singur Mahamaya High School and Passed Madhyamik Examination in 2019 with 65% marks.Then I'm started my Higher Secondary Education from Singur Mahamaya High School and passed my HS examination in 2021 with 75% marks. Then I was got admission at Techno India Hooghly in Month of July,2021.I started my BCA degree from Techno India Hooghly Campus.I am a Motivated BCA student with a current CGPA of 8.39.I love to do with a passion for web development. Currently, I am passionate my skills in full-stack web development.Effective communicator in English, Bengali, and Hindi. Dedicated to continuous learning and self-improvement. I have also experienced in Programming Language like C,Java,Python. Also experienced in HTML 5, CSS 3, Java Script so on.I have successfully completed several projects using these technologies. That's all about myself, Thank You. </span>"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
